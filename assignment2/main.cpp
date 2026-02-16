defac33241b78189f54bcc4f87fd8d1b10744809
Tiep Tran <51280433+polskiTran@users.noreply.github.com>
2026-02-16 14:34:02 -0500

    assignment2 - add the remaining math functions to main.cpp

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -8,10 +8,23 @@ void MathResult2(int result) {
     std::cout << "  (>) Divide by 3: " << result/3 << std::endl;
 }
 int main() {
-    std::cout << "Run MathLibrary with Callbacks..." << std::endl;
+    std::cout << "Run MathLibrary with Callbacks MathResult1..." << std::endl;
+    // MathResult1
     Add(10, 20, MathResult1);
+    Subtract(10, 20, MathResult1);
     Multiply(5, 6, MathResult1);
+    Divide(10, 2, MathResult1);
+    Modulo(10, 3, MathResult1);
+    Cube(5, MathResult1);
+    Square(5, MathResult1);
+
+    // MathResult2
+    std::cout << "Run MathLibrary with Callbacks MathResult2..." << std::endl;
     Add(10, 20, MathResult2);
     Multiply(5, 6, MathResult2);
+    Divide(10, 2, MathResult2);
+    Modulo(10, 3, MathResult2);
+    Cube(5, MathResult2);
+    Square(5, MathResult2);
     return 0;
 }
